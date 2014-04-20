8540b60b20fe423f2561ae8cd5c391cfd9dbdfff
neil patel <neil@myindianweddingapp.com>
2014-04-20 19:37:19 +0100

    bundle install , all ready to push to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Demo::Application.routes.draw do
+  
+  root 'pages#home'
+
+  get "about" => "pages#about"
+  get "contact" => "pages#contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
